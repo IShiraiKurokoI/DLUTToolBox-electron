04314f3542cfdc3c1393c10f37123e7fbb718d34
Shirai_Kuroko <2321047703@qq.com>
2023-11-14 17:35:36 +0800

    settings

--- js/settings.js
+++ js/settings.js
@@ -1,9 +1,17 @@
 const Store = require('electron-store');
+const $ = require("jquery");
 const store = new Store();
-$('#settings').on('submit', function(event) {
-    event.preventDefault()
-    store.set('username', $('#username').val());
-    store.set('password', $('#password').val());
-    const toast = bootstrap.Toast.getOrCreateInstance(document.getElementById('Toast'))
-    toast.show()
-});
+
+window.onload = function() {
+    $('#username').value=store.get("username")
+    $('#password').value=store.get("password")
+    $('#settings').on('submit', function(event) {
+        event.preventDefault()
+        store.set('username', $('#username').val());
+        store.set('password', $('#password').val());
+        const NOTIFICATION_TITLE = '保存成功'
+        const NOTIFICATION_BODY = '配置已经成功保存！'
+        new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY }).onclick =
+            () => console.log("")
+    });
+};
