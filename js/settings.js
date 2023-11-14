3e56ae248eb1cdf2f5310238b9d577f6feb71ca7
Shirai_Kuroko <2321047703@qq.com>
2023-11-14 17:39:49 +0800

    settings

--- js/settings.js
+++ js/settings.js
@@ -3,15 +3,16 @@ const $ = require("jquery");
 const store = new Store();
 
 window.onload = function() {
-    $('#username').value=store.get("username")
-    $('#password').value=store.get("password")
+    $('#username').val(store.get("username"))
+    $('#password').val(store.get("password"))
     $('#settings').on('submit', function(event) {
-        event.preventDefault()
         store.set('username', $('#username').val());
         store.set('password', $('#password').val());
         const NOTIFICATION_TITLE = '保存成功'
         const NOTIFICATION_BODY = '配置已经成功保存！'
         new Notification(NOTIFICATION_TITLE, { body: NOTIFICATION_BODY }).onclick =
             () => console.log("")
+
+        event.preventDefault()
     });
 };
