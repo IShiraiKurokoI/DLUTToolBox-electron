f337f67a4d039323c757018ed84e752279b6a0d4
Shirai_Kuroko <2321047703@qq.com>
2023-11-14 20:44:22 +0800

    weather

--- js/main.js
+++ js/main.js
@@ -17,9 +17,21 @@ window.onload = function() {
     });
 
 
+    const weather = $("#weather")
+    weather.on('did-navigate', function (event) {
+        var currentURL = weather[0].getURL();
+        if (currentURL.includes("cas/login?")){
+            classtable[0].executeJavaScript("un.value='" + store.get("username") + "';pd.value='" + store.get("password") + "';rememberName.checked='checked';login()",false);
+        }
+        if (currentURL==="http://www.weather.com.cn/"){
+            weather[0].executeJavaScript("document.body.innerHTML=document.getElementsByClassName('myWeather')[0].outerHTML;document.getElementsByClassName('myWeatherTop')[0].outerHTML='';document.getElementsByTagName('a')[0].outerHTML='';document.body.style='overflow:hidden;background-color:transparent';document.getElementsByTagName('div')[0].style='background-color:transparent';",false);
+        }
+    });
+
+
     const workframe = $("#workframe")
     workframe.on('did-navigate', function (event) {
-        const currentURL = workframe[0].getURL();
+        var currentURL = workframe[0].getURL();
         if (currentURL.includes("/cas/login?")){
             workframe[0].executeJavaScript("un.value='" + store.get("username") + "';pd.value='" + store.get("password") + "';rememberName.checked='checked';login()",false);
         }
