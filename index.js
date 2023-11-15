9466352a164eeb4975b60b7b3525ed741e26c440
Shirai_Kuroko <2321047703@qq.com>
2023-11-15 20:24:26 +0800

    exam page study page

--- index.js
+++ index.js
@@ -6,7 +6,25 @@ Store.initRenderer()
 const functions = [
     path.join(__dirname, 'monitor.html'),
     "https://sso.dlut.edu.cn/cas/login?service=http%3A%2F%2F172.20.30.2%3A8080%2FSelf%2Fsso_login%3Flogin_method%3D1",
-    "https://api.m.dlut.edu.cn/oauth/authorize?client_id=19b32196decf419a&redirect_uri=https%3A%2F%2Fcard.m.dlut.edu.cn%2Fhomerj%2FopenRjOAuthPage&response_type=code&scope=base_api&state=weishao"
+    "https://api.m.dlut.edu.cn/oauth/authorize?client_id=19b32196decf419a&redirect_uri=https%3A%2F%2Fcard.m.dlut.edu.cn%2Fhomerj%2FopenRjOAuthPage&response_type=code&scope=base_api&state=weishao",
+    "https://sso.dlut.edu.cn/cas/login?from=rj&service=https%3A%2F%2Fsso.dlut.edu.cn%2Fcas%2Flogin%3Fservice%3Dhttps%253A%252F%252Fehall.dlut.edu.cn%252Ffp%252FvisitService%253Fservice_id%253Dca2b52e6-1145-4b63-9ea9-e443b376da0d",
+    "https://webvpn.dlut.edu.cn/http/57787a7876706e323032336b6579402472000514b2079f5bbe02ff3c84f58629/student/ucas-sso/login?filter=app&from=rj",
+    "https://api.m.dlut.edu.cn/login?client_id=9qXqHnRQuhhViycC&redirect_uri=https%3a%2f%2flightapp.m.dlut.edu.cn%2fcheck%2fschcalendar&response_type=code&scope=base_api&state=dlut",
+    "https://webvpn.dlut.edu.cn/http/57787a7876706e323032336b6579402472000514b2079f5bbe02ff3c84f58629/student/ucas-sso/login?filter=app&from=rj",
+    "https://webvpn.dlut.edu.cn/http/57787a7876706e323032336b6579402472000514b2079f5bbe02ff3c84f58629/student/ucas-sso/login?filter=app&from=rj",
+    "https://webvpn.dlut.edu.cn/http/57787a7876706e323032336b6579402472000514b2079f5bbe02ff3c84f58629/student/ucas-sso/login?filter=app&from=rj",
+    "https://webvpn.dlut.edu.cn/http/57787a7876706e323032336b6579402472000514b2079f5bbe02ff3c84f58629/student/ucas-sso/login?filter=app&from=rj",
+    "https://webvpn.dlut.edu.cn/http/57787a7876706e323032336b6579402472000514b2079f5bbe02ff3c84f58629/student/ucas-sso/login?filter=app&from=rj",
+    "https://webvpn.dlut.edu.cn/http/57787a7876706e323032336b6579402472000514b2079f5bbe02ff3c84f58629/student/ucas-sso/login?filter=app&from=rj",
+    "https://api.m.dlut.edu.cn/login?client_id=9qXqHnRQuhhViycC&redirect_uri=https%3a%2f%2flightapp.m.dlut.edu.cn%2fcheck%2fcourseschedule&response_type=code",
+    "https://webvpn.dlut.edu.cn/http/57787a7876706e323032336b6579402472000514b2079f5bbe02ff3c84f58629/student/ucas-sso/login?filter=app&from=rj",
+    "https://webvpn.dlut.edu.cn/http/57787a7876706e323032336b6579402472000514b2079f5bbe02ff3c84f58629/student/ucas-sso/login?filter=app&from=rj",
+    "https://webvpn.dlut.edu.cn/http/57787a7876706e323032336b6579402472000514b2079f5bbe02ff3c84f58629/student/ucas-sso/login?filter=app&from=rj",
+    "https://api.m.dlut.edu.cn/login?client_id=9qXqHnRQuhhViycC&redirect_uri=https%3a%2f%2flightapp.m.dlut.edu.cn%2fcheck%2fnotice&response_type=code&scope=base_api&state=dlut",
+    "https://webvpn.dlut.edu.cn/http/57787a7876706e323032336b6579402472000514b2079f5bbe02ff3c84f58629/student/ucas-sso/login?filter=app&from=rj",
+    "https://api.m.dlut.edu.cn/login?client_id=9qXqHnRQuhhViycC&redirect_uri=https%3a%2f%2flightapp.m.dlut.edu.cn%2fcheck%2femptyclassroom&response_type=code&scope=base_api&state=dlut",
+    "https://sso.dlut.edu.cn/cas/login?service=https%3A%2F%2Fehall.dlut.edu.cn%2Ffp%2Fview%3Fm%3Dfp#act=fp/formProcess/graduate_student",
+
 ]
 
 function createWindow() {
@@ -94,6 +112,10 @@ app.whenReady().then(() => {
                 childWin.center()
                 childWin.webContents.userAgent="Mozilla/5.0 (Linux; Android 10; EBG-AN00 Build/HUAWEIEBG-AN00; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/83.0.4103.106 Mobile Safari/537.36 weishao(3.2.2.74627)"
                 break;
+            case 12:
+                childWin.setSize(400,700)
+                childWin.center()
+                break;
             default:
                 break;
         }
@@ -113,6 +135,59 @@ app.whenReady().then(() => {
                 case 2:
                     if (currentURL.includes("https://card.m.dlut.edu.cn/virtualcard/openVirtualcard?")){
                         childWin.webContents.executeJavaScript("document.getElementsByClassName('code')[0].className=''")
+                    }
+                    break;
+                case 6:
+                    if (currentURL.includes("/student/home")){
+                        childWin.webContents.executeJavaScript("window.location.href='/student/for-std/course-select'")
+                    }
+                    if (currentURL.includes("for-std/course-select")){
+                        childWin.webContents.executeJavaScript("var num=document.getElementsByClassName('get-into').length;for(i = 0; i < num; i++){document.getElementsByClassName('get-into')[i].target = '_blank'}")
+                    }
+                    break;
+                case 7:
+                    if (currentURL.includes("/student/home")){
+                        childWin.webContents.executeJavaScript("window.location.href='/student/for-std/evaluation/summative'")
+                    }
+                    break;
+                case 8:
+                    if (currentURL.includes("/student/home")){
+                        childWin.webContents.executeJavaScript("window.location.href='/student/for-std/exam-arrange'")
+                    }
+                    break;
+                case 9:
+                    if (currentURL.includes("/student/home")){
+                        childWin.webContents.executeJavaScript("window.location.href='/student/for-std/exam-delay-apply'")
+                    }
+                    break;
+                case 10:
+                    if (currentURL.includes("/student/home")){
+                        childWin.webContents.executeJavaScript("window.location.href='/student/for-std/grade/sheet'")
+                    }
+                    break;
+                case 11:
+                    if (currentURL.includes("/student/home")){
+                        childWin.webContents.executeJavaScript("window.location.href='/student/for-std/course-select-apply'")
+                    }
+                    break;
+                case 13:
+                    if (currentURL.includes("/student/home")){
+                        childWin.webContents.executeJavaScript("window.location.href='/student/for-std/course-table'")
+                    }
+                    break;
+                case 14:
+                    if (currentURL.includes("/student/home")){
+                        childWin.webContents.executeJavaScript("window.location.href='/student/for-std/adminclass-course-table'")
+                    }
+                    break;
+                case 15:
+                    if (currentURL.includes("/student/home")){
+                        childWin.webContents.executeJavaScript("window.location.href='/student/for-std/program-completion-preview'")
+                    }
+                    break;
+                case 17:
+                    if (currentURL.includes("/student/home")){
+                        childWin.webContents.executeJavaScript("window.location.href='/student/for-std/lesson-search'")
                     }
                     break;
                 default:
